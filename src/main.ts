bcf2557fbd26856885613a9c2e4df19f2f6cdb78
LautaroZalazar <lautyzalazar10@gmail.com>
2024-05-17 21:17:34 -0300

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  /* app.enableCors(corsOptions); */
+  app.enableCors(corsOptions);
   app.use(json({ limit: '5mb' }));
   app.use(urlencoded({ extended: true, limit: '5mb' }));
   app.useGlobalPipes(new ValidationPipe());
@@ -29,9 +29,9 @@ bootstrap()
   .then(() => {
     console.log(
       'Listening on: http://localhost:' +
-        config().app.app_port +
-        '/' +
-        config().app.app_global_prefix,
+      config().app.app_port +
+      '/' +
+      config().app.app_global_prefix,
     );
     console.log('Server started successfully 🎸 ');
   })
